c118121999895ed75d1abd69c56df58c1d46aaa5
unknown <bruna.dalmagro23re20@gmail.com>
2024-03-07 14:09:36 -0300

    Exercisms

--- 4. Schema database/--Tabela Sql.sql
+++ 4. Schema database/--Tabela Sql.sql
@@ -19,41 +19,37 @@
 -- Comando DELETE - Por fim, o comando DELETE é aplicado para excluir o registro com ID igual a 2 da tabela "clientes".
 
 --Exemplo:
---(link sqlfiddle:) https://sqlfiddle.com/sqlite/online-compiler?id=795874e5-005a-49c4-8e73-ee00da5723c0
+--(link sqlfiddle para executar o programa:) https://sqlfiddle.com/sqlite/online-compiler?id=9792bd7d-926b-412b-af35-5c25fa757db7
 
--- Comando CREATE TABLE - Criando uma tabela chamada "dados" com as colunas "id" (chave primária), "comando", "coluna1", "coluna2" e "valor_condicao".
-CREATE TABLE dados (
+CREATE TABLE clientes (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  comando TEXT,
-  coluna1 TEXT,
-  coluna2 TEXT,
-  valor_condicao TEXT
+  nome TEXT,
+  email TEXT,
+  idade INTEGER
 );
 
 -- Comando INSERT - insere registros na tabela "dados".
-INSERT INTO dados (comando, coluna1, coluna2, valor_condicao)
-VALUES ('SELECT', 'valor1', 'valor2', 'valor_condicao1');
-
-INSERT INTO dados (comando, coluna1, coluna2, valor_condicao)
-VALUES ('INSERT', 'valor3', 'valor4', 'valor_condicao2');
-
-INSERT INTO dados (comando, coluna1, coluna2, valor_condicao)
-VALUES ('UPDATE', 'valor5', 'valor6', 'valor_condicao3');
-
-INSERT INTO dados (comando, coluna1, coluna2, valor_condicao)
-VALUES ('DELETE', 'valor7', 'valor8', 'valor_condicao4');
+INSERT INTO clientes (nome, email, idade)
+VALUES ('Eveline', 'eveline@email.com', 20), 
+('Carlos', 'carlo@email.com', 32), 
+('Valentine', 'valentine@email', 27), 
+('Leon', 'leon@email', 28);
 
 -- Comando SELECT - Seleciona todos os dados da tabela "dados".
-SELECT * FROM dados;
+SELECT * FROM clientes;
 
 -- Comando UPDATE - Atualiza a coluna "coluna2" para 'novo_valor2' no registro com o ID igual a 1 na tabela "dados".
-UPDATE dados
-SET coluna2 = 'novo_valor2'
-WHERE id = 1;
-
--- Comando DELETE - Exclui o registro com ID igual a 2 da tabela "dados".
-DELETE FROM dados
+UPDATE clientes
+SET nome = 'Carlo'
 WHERE id = 2;
 
+UPDATE clientes
+SET idade = 29
+WHERE id = 3;
+
+-- Comando DELETE - Exclui o registro com ID igual a 2 da tabela "dados".
+DELETE FROM clientes
+WHERE id = 1;
+
 -- Comando SELECT - Seleciona todos os dados da tabela "dados" após as operações de UPDATE e DELETE.
-SELECT * FROM dados;
+SELECT * FROM clientes;
